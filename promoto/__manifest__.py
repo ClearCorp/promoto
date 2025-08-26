583765da67ad108af6575fd3e22f04bf81fe86c1
avilavenegas <avilavenegas@gmail.com>
2025-08-26 11:59:22 -0600

    v14

---
+++ promoto/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Module App for Promoto',
+    'summary': 'Costa Rica Localization for EDI documents',
+    'version': '14.0.1.0.0',
+    'category': 'Installer',
+    'author': 'Vauxoo',
+    'license': 'LGPL-3',
+    'depends': [
+        'l10n_cr_edi',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'pre_init_hook': 'pre_init_hook',
+}
